0e3cb62eb0e35d5cf5eff7e49a9bf81e0be8bc27
Lee Ji Heok <olzlgur@naver.com>
2021-08-13 21:32:09 +0900

    글쿤

--- firstproject/settings.py
+++ firstproject/settings.py
@@ -128,7 +128,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'firstapp', 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static_cdn', 'media_root')
 MEDIA_URL = '/media/'
 
 

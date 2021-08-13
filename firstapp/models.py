eee5b3009df63555931a1b15ca7e87a445aa06ba
Lee Ji Heok <olzlgur@naver.com>
2021-08-14 02:52:07 +0900

    이렇게?

--- firstapp/models.py
+++ firstapp/models.py
@@ -27,7 +27,7 @@ class Profile(models.Model):
     profile_photo = models.ImageField(upload_to='profile/',blank=True)          # 값을 채워넣지 않아도 되는 속성.
 
 class Comment(models.Model):
-    post = models.ForeignKey(Blog, on_delete=models.CASCADE, null=True)
+    post = models.ForeignKey(Blog, on_delete=models.CASCADE, null=True) 
     body = models.TextField(max_length=200)
     date = models.DateTimeField(default=timezone.now)
 
